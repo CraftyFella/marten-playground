b7e2e7a841fad9899f8cc481c9ed5cc407feb43b
dc154p <dcraft2@wayfair.com>
2022-01-28 15:18:42 +0000

    Using Hot/Cold and the idea from post
    https://event-driven.io/en/integrating_Marten/

--- producer/Program.cs
+++ producer/Program.cs
@@ -2,6 +2,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using common;
+using Marten;
+using Marten.Events.Daemon.Resiliency;
+using Marten.Events.Projections;
+using Weasel.Postgresql;
 
 namespace producer
 {
@@ -10,7 +14,18 @@ namespace producer
         static async Task Main(string[] args)
         {
             var stream = Guid.Parse("cb90cb3f-867f-4649-a991-5b84ab526bc1");
-            var documentStore = DocumentStoreFactory.Create(args.Any());
+            const string connectionString =
+                "PORT = 5432; HOST = 127.0.0.1; TIMEOUT = 15; POOLING = True; MINPOOLSIZE = 1; MAXPOOLSIZE = 100; COMMANDTIMEOUT = 20; DATABASE = 'postgres'; PASSWORD = 'Password12!'; USER ID = 'postgres'";
+            
+            var store = DocumentStore.For(options =>
+            {
+                options.Connection(connectionString);
+                options.AutoCreateSchemaObjects = args.Any() ? AutoCreate.CreateOnly : AutoCreate.None;
+                options.DatabaseSchemaName = "EventStore";
+                options.Events.DatabaseSchemaName = "EventStore";
+                options.Events.AddEventType(typeof(AnEvent));
+            });
+            var documentStore = (DocumentStore)store;
             var session = documentStore.OpenSession();
             var eventStore = session.Events;
 
